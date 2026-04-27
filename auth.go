0ce3f2cb4ed4c2504ec35236d5a188cde838a84d
Aleksandr Koltunov <firstsano@gmail.com>
2026-04-27 12:44:56 +0500

    feat(LSN-4.4): remove redundant logs

--- auth.go
+++ auth.go
@@ -57,7 +57,6 @@ func (s *server) validatePassword(password, stored string) (bool, error) {
 		return false, nil
 	}
 	if err != nil {
-		s.logger.Error("error validating password", "error", err)
 		return false, err
 	}
 	return true, nil
